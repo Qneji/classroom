ff5a6a51060c9b262001c9117912867d42257c7d
kenny <kennedy_ken99@hotmail.com>
2026-05-15 17:08:37 -0300

    implementando a pasta hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./global.css"
 
 import { Button } from "./components/button";
+import { useMessage } from "./hooks/useMessage"
 
 import styles from "./app.module.css"
 
 export function App() {
+  const { show } = useMessage({ age: 26, name: "Kenny" })
+
   return (
     <div className={styles.container} >
-      <Button name="Adicionar" />
+      <Button name="Adicionar" onClick= { () =>  show("Mensagem personalizada do meu hook!") }/>
       <span>0</span>
       <Button name="Remover" />
     </div>
