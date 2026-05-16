14fec76dfb78884556f40d3706b2fda16dfc945f
kenny <kennedy_ken99@hotmail.com>
2026-05-16 15:01:39 -0300

    Aprendendo useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import "./global.css"
+import { useState } from "react";
 
 import { Button } from "./components/button";
-import { useMessage } from "./hooks/useMessage"
+// import { useMessage } from "./hooks/useMessage"
 
 import styles from "./app.module.css"
 
 export function App() {
-  const { show } = useMessage({ age: 26, name: "Kenny" })
+  const [count, setCount] = useState(0)
+
+  // const { show } = useMessage({ age: 26, name: "Kenny" })
+
+  function handleAdd() {
+    setCount(count + 1)
+  }
+
+  function handleRemove() {
+    setCount(count - 1)
+  }
 
   return (
     <div className={styles.container} >
-      <Button name="Adicionar" onClick= { () =>  show("Mensagem personalizada do meu hook!") }/>
-      <span>0</span>
-      <Button name="Remover" />
+      <Button name="Adicionar" onClick={ handleAdd }/>
+      <span>{ count }</span>
+      <Button name="Remover" onClick={ handleRemove }/>
     </div>
   );
 }
